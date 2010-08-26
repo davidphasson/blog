8f6c9cf421491492ecd30a418a49011d0dc6984c
David Hasson <dph@funtaff.net>
2010-08-25 22:29:49 -0700

    Initial Rails Deployment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b181bce0c17d17bf7543d5215181931eb8d431184d49ebdd7b39482deacfe32d560c60f13ebe80f1d4726207d4da4aa8ec37abf664f3e73ab8d82f52eb3a06a8'
